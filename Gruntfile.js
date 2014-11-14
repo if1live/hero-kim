99e521b07b118f26d8abdbfb0df844a290ed47f7
if1live <libsora25@gmail.com>
2014-11-14 21:13:47 +0900

    support typescript definition file

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,15 +14,42 @@ module.exports = function(grunt) {
             "phaser/build/phaser.js",
             "phaser/build/phaser.min.js",
             "phaser/build/phaser.map",
-            "phaser/build/phaser.d.ts"
+            "phaser/build/phaser.d.ts",
+
+            "chai/chai.js",
+
+            "mocha/mocha.js",
+            "mocha/mocha.css"
           ]
         }
+      }
+    },
+    tsd: {
+      refresh: {
+        options: {
+          // execute a command
+          command: 'reinstall',
+          //optional: always get from HEAD
+          latest: true,
+          // specify config file
+          config: 'tsd.json',
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, flatten: true, src: ['typings/chai/*.ts'], dest: 'libs/'},
+          {expand: true, flatten: true, src: ['typings/mocha/*.ts'], dest: 'libs/'}
+        ]
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-bowercopy");
+  grunt.loadNpmTasks('grunt-tsd');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s)
-  grunt.registerTask("default", ["bowercopy"]);
+  grunt.registerTask("default", ["tsd", "bowercopy", 'copy']);
 };
