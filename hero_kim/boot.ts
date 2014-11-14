80c459ce71ab6c6fe040ffc713ce925269c20e3a
if1live <libsora25@gmail.com>
2014-11-14 21:51:25 +0900

    support many file

---
+++ hero_kim/boot.ts
@@ -0,0 +1,11 @@
+﻿module HeroKim {
+    export class Boot extends Phaser.State {
+        preload() {
+            this.load.image('preloadBar', 'assets/loader.png');
+        }
+
+        create() {
+            alert('hello world');
+        }
+    }
+}
